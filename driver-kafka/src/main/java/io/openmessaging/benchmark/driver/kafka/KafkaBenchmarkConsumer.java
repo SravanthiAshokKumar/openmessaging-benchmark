95b89dbd6e2aa89426ba3bc30c2334cd26799cfe
Manasvini <msethuraman3@gatech.edu>
2020-03-18 13:30:49 +0000

    print to log

--- driver-kafka/src/main/java/io/openmessaging/benchmark/driver/kafka/KafkaBenchmarkConsumer.java
+++ driver-kafka/src/main/java/io/openmessaging/benchmark/driver/kafka/KafkaBenchmarkConsumer.java
@@ -96,6 +96,9 @@ public class KafkaBenchmarkConsumer implements BenchmarkConsumer {
                     ConsumerRecords<String, byte[]> records = consumer.poll(100);
 
                     for (ConsumerRecord<String, byte[]> record : records) {
+                        if( record.topic().equals(subscription) !=true){
+                            log.error("consumer topic = {}, subscribed topic={}", record.topic(), subscription);
+}
                         callback.messageReceived(record.value(), record.timestamp());
 
                         offsetMap.put(new TopicPartition(record.topic(), record.partition()),
